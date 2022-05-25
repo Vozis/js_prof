e9b326e2ef649c84edc381b285ab1632b7a32893
ilyaSizov <sizov.ilya1996@gmail.com>
2022-05-25 17:10:31 +0300

    update homework v_1

--- lesson-1/js/main.js
+++ lesson-1/js/main.js
@@ -7,21 +7,25 @@ const products = [
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (item) => {
-  this.title = item.title;
-  this.price = item.price;
-  this.id = item.id;
+  // this.title = item.title;
+  // this.price = item.price;
+  // this.id = item.id;
   return `<div class="product-item">
-                <img src="./img/${this.id}.jpeg" alt="image">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
+                <img src="./img/${item.id}.jpeg" alt="image">
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`;
 };
 
 const renderPage = (list) => {
-  const productsList = list.map((item) => renderProduct(item));
-  console.log(productsList);
-  document.querySelector(".products").innerHTML = productsList.join("");
+  // const productsList = list.map((item) => renderProduct(item));
+  // console.log(productsList);
+  // document.querySelector(".products").innerHTML = productsList.join("");
+
+  document.querySelector(".products").innerHTML = list
+    .map((item) => renderProduct(item))
+    .join("");
 };
 
 renderPage(products);
